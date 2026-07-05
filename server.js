cab87168c4b7a86ba1c403908df3971f97f780bd
herculesinfantaria51-spec <herculesinfantaria51@gmail.com>
2026-07-05 13:08:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const port = process.env.PORT || 3000;
 
 // Conexão direta e pura com o banco
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 app.use(cors());
